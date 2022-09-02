1d3560ae8860951e2cd7c8417676dd65f934b601
Souza <joao.souza@capgemini.com>
2022-09-02 09:51:55 -0300

    UPDATE

--- src/config/server.ts
+++ src/config/server.ts
@@ -12,7 +12,7 @@ const app = express()
 // Set Middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors(corsConfig))
+app.use(cors())
 app.use(morgan('dev'))
 app.use('/', router)
 app.use(BugLaucher)
